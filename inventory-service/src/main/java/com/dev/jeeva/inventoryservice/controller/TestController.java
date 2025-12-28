8f5f415e0c02c057d0c9c36bfec107a19894ff5f
Jeeva <palanijeeva2004@gmail.com>
2025-12-28 18:52:21 +0530

    first commit

---
+++ inventory-service/src/main/java/com/dev/jeeva/inventoryservice/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.dev.jeeva.inventoryservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Inventory Service! The harvest is coming.";
+    }
+}
